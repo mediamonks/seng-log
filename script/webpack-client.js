bfb1a2dda2c95d993f945ddd6001f83f796bc068
Victor Garrido <v.garrido@mediamonks.com>
2016-10-12 10:49:19 +0200

    Refactor in webpack-client

--- script/webpack-client.js
+++ script/webpack-client.js
@@ -1,15 +1,15 @@
+
 const webpack = require("webpack");
 const webpackDevServer = require('webpack-dev-server');
 const path = require('path');
 
 var baseConfig = require('../config/webpack.config.dist');
-
 var browser = baseConfig();
 browser.output.libraryTarget = "var";
 browser.output.filename = "./dist/seng-log.js";
-browser.output.path = path.resolve(__dirname, '../dist');
-browser.output.publicPath = '/';
+browser.output.path = path.join(__dirname, '../dist');
 browser.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
+browser.devtool = 'source-map';
 browser.watch = true;
 browser.progress = true;
 browser.keepalive = true;
